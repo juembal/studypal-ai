4971069f28c44eba20b9c01b8ccbc189f61cb98e
juembal <joembalingit15@gmail.com>
2025-08-14 23:51:55 +0800

    jlk

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove static export for now to test basic deployment
   trailingSlash: true,
   images: {
     unoptimized: true
